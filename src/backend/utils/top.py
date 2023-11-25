1d03c1ce152e771c082cf132ba350e46464e8080
sarvensis <ivank_ri@mail.ru>
2023-11-25 11:39:56 +0500

    Help model2

--- src/backend/utils/top.py
+++ src/backend/utils/top.py
@@ -2,12 +2,75 @@ import pickle
 import random
 from pathlib import Path
 from typing import List
-
 from pydantic import BaseModel, Field, TypeAdapter
+
+# from app.lifespan import global_objs
 
 
 path = Path(__file__).parent.absolute()
 path = path / "backfill.pickle"
+
+
+def match_dates(query: str, video: str) -> float:
+    res_query = date_parser.es.findall(query)
+    spans_query = [_.span for _ in res_query]
+
+    res_video = date_parser.es.findall(video)
+    spans_video = [_.span for _ in res_video]
+
+    if (not spans_query and spans_video) or (spans_query and not spans_video):
+        return 0.0
+
+    if not spans_query and not spans_video:
+        return -1.0
+
+    spans_query = query[spans_query[0].start : spans_query[0].stop]
+    spans_video = video[spans_video[0].start : spans_video[0].stop]
+
+    if spans_query == spans_video:
+        return 1.0
+
+    return len(set(spans_query) & set(spans_video)) / max(
+        len(set(spans_query)), len(set(spans_query))
+    )
+
+
+def match_episodes(query: str, video: str) -> float:
+    res_query = episode_re.search(query)
+    res_video = episode_re.search(query)
+
+    if (not res_query and res_video) or (res_query and not res_video):
+        return 0.0
+
+    if not res_query and not res_video:
+        return -1.0
+
+    res_query = number_re.findall(res_query.group(0))[0]
+    res_video = number_re.findall(res_video.group(0))[0]
+
+    if res_query == res_video:
+        return 1.0
+
+    return 0.0
+
+
+def match_season(query: str, video: str) -> float:
+    res_query = season_re.search(query)
+    res_video = season_re.search(query)
+
+    if (not res_query and res_video) or (res_query and not res_video):
+        return 0.0
+
+    if not res_query and not res_video:
+        return -1.0
+
+    res_query = number_re.findall(res_query.group(0))[0]
+    res_video = number_re.findall(res_video.group(0))[0]
+
+    if res_query == res_video:
+        return 1.0
+
+    return 0.0
 
 
 class VideoDetailTopIn(BaseModel):
